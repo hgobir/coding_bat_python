80033f69ca1238e8ac1dfbce5566d9bc633f8549
Hamza Gobir <hgobir@googlemail.com>
2022-02-09 14:54:14 +0000

    finished list1 problems

--- main/list1_problems/__init__.py
+++ main/list1_problems/__init__.py
@@ -32,20 +32,47 @@ def sum3(nums):
 def rotate_left3(nums):
     return [nums[1], nums[2], nums[0]]
 
-#print(rotate_left3([1,2,3]))
+
+# print(rotate_left3([1,2,3]))
 
 
 def reverse3(nums):
-    return [ nums[2], nums[1], nums[0] ]
-
-print(reverse3([1,2,3]))
+    return [nums[2], nums[1], nums[0]]
 
 
+# print(reverse3([1,2,3]))
+
+def max_end3(nums):
+    return [nums[0], nums[0], nums[0]] if nums[0] > nums[2] else [nums[2], nums[2], nums[2]]
+
+
+# print(max_end3([1,2,3]))
+
+
+def middle_Waysum2(nums):
+    if len(nums) == 0:
+        return 0
+    return nums[0] + nums[1] if len(nums) >= 2 else nums[0]
+
+
+#print(sum2([]))
+
+
+def middle_Way(a, b):
+    return [a[1], b[1]]
+
+
+#print(middle_Way([1,2,3], [4,5,6]))
 
 
 
+def make_ends(nums):
+    return [nums[0], nums[-1]] if len(nums) > 1 else [nums[0], nums[0]]
+
+#print(make_ends([1]))
 
 
+def has23(nums):
+    return 2 in nums or 3 in nums
 
-
-
+print(has23([1]))
